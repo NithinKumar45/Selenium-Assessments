5f8896029832ba3b334af3646e7d4d737bbfee4e
NITHIN KUMAR G <167528883+NithinKumar45@users.noreply.github.com>
2026-09-17 09:35:59 +0530

    Add files via upload

---
+++ day_8/Pom_Login.java
@@ -0,0 +1,41 @@
+package assessments.day_8;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Pom_Login {
+	WebDriver driver;
+	public Pom_Login(WebDriver driver) {
+		this.driver= driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	//username
+	@FindBy(xpath = "//input[@name=\"username\"]")
+	private WebElement un;
+	
+	//password
+	@FindBy(xpath = "//input[@name=\"password\"]")
+	private WebElement pwd;
+	
+	//login button
+	@FindBy(xpath = "//button[@type=\"submit\"]")
+	private WebElement lgbutton;
+	
+	
+
+	public void getLgbutton() {
+		lgbutton.click();
+	}
+	
+	public void getUn(String value) {
+		un.sendKeys(value);
+	}
+	
+	public void getPwd(String value) {
+		pwd.sendKeys(value);
+	}
+	
+}
